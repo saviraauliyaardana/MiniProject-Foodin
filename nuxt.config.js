d9cb8ad2858b86a18d450274d6f9520d02fa3cf5
Savira Auliya Ardana <savirauliya10@gmail.com>
2022-05-21 12:22:25 +0700

    feat : delete typescript

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
+    
     "nuxt-graphql-request",
   ],
 
